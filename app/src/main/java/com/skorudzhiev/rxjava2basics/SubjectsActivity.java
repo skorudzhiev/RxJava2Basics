c27532086f0564adec5fac94750cd4a3ab51f739
skorudzhiev <skorudzhiev@gmail.com>
2018-08-27 09:27:09 +0300

    feat: implementation of Replay, Behavior and Async Subjects in the same activity used for the Publish subject

--- app/src/main/java/com/skorudzhiev/rxjava2basics/SubjectsActivity.java
+++ app/src/main/java/com/skorudzhiev/rxjava2basics/SubjectsActivity.java
@@ -4,18 +4,20 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.subjects.AsyncSubject;
+import io.reactivex.subjects.BehaviorSubject;
 import io.reactivex.subjects.PublishSubject;
+import io.reactivex.subjects.ReplaySubject;
 
 public class SubjectsActivity extends AppCompatActivity {
 
     private static final String TAG = "TAG";
-    Button button;
+    Button publishButton, replayButton, behaviorButton, asyncButton;
     TextView textView;
 
 
@@ -24,22 +26,25 @@ public class SubjectsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.subjects_activity);
 
-        button = findViewById(R.id.subj_button);
+        publishButton = findViewById(R.id.publish_subject);
+        replayButton = findViewById(R.id.replay_button);
+        behaviorButton = findViewById(R.id.behavior_button);
+        asyncButton = findViewById(R.id.async_button);
         textView = findViewById(R.id.subject_text_view);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                doSomeWork();
-            }
-        });
+        publishButton.setOnClickListener(view -> publishSomeWork());
+        replayButton.setOnClickListener(view -> replaySomeWork());
+        behaviorButton.setOnClickListener(view -> behaviorSomeWork());
+        asyncButton.setOnClickListener(view -> asyncSomeWork());
+
     }
 
     /**
      * PublishSubject emits to an observer only those items that are emitted
      * by the source Observable, subsequent to the time of the subscription.
      */
-    private void doSomeWork() {
+    private void publishSomeWork() {
+        textView.setText("");
 
         PublishSubject<Integer> source = PublishSubject.create();
 
@@ -58,6 +63,85 @@ public class SubjectsActivity extends AppCompatActivity {
         source.onComplete();
 
     }
+
+    /**
+     * ReplaySubject emits to any observer all of the items that were emitted
+     * by the source Observable, regardless of when the observer subscribes.
+     */
+    private void replaySomeWork() {
+        textView.setText("");
+
+        ReplaySubject<Integer> source = ReplaySubject.create();
+
+        source.subscribe(getFirstObserver()); // it will get 1, 2, 3, 4
+
+        source.onNext(1);
+        source.onNext(2);
+        source.onNext(3);
+        source.onNext(4);
+        source.onComplete();
+
+        /*
+         * it will emit 1, 2, 3, 4 for second observer also as we have used replay
+         */
+        source.subscribe(getSecondObserver());
+
+    }
+
+    /**
+     * When an observer subscribes to a BehaviorSubject, it begins by emitting the item most
+     * recently emitted by the source Observable (or a seed/default value if none has yet been
+     * emitted) and then continues to emit any other items emitted later by the source Observable(s).
+     * It is different from Async Subject as async emits the last value (and only the last value)
+     * but the Behavior Subject emits the last and the subsequent values also.
+     */
+    private void behaviorSomeWork() {
+        textView.setText("");
+
+        BehaviorSubject<Integer> source = BehaviorSubject.create();
+
+        source.subscribe(getFirstObserver()); // it will get 1, 2, 3, 4 and onComplete
+
+        source.onNext(1);
+        source.onNext(2);
+        source.onNext(3);
+
+        /*
+         * it will emit 3(last emitted), 4 and onComplete for second observer also.
+         */
+        source.subscribe(getSecondObserver());
+
+        source.onNext(4);
+        source.onComplete();
+
+    }
+
+    /**
+     * An AsyncSubject emits the last value (and only the last value) emitted by the source
+     * Observable, and only after that source Observable completes. (If the source Observable
+     * does not emit any values, the AsyncSubject also completes without emitting any values.)
+     */
+    private void asyncSomeWork() {
+        textView.setText("");
+
+        AsyncSubject<Integer> source = AsyncSubject.create();
+
+        source.subscribe(getFirstObserver()); // it will emit only 4 and onComplete
+
+        source.onNext(1);
+        source.onNext(2);
+        source.onNext(3);
+
+        /*
+         * it will emit 4 and onComplete for second observer also.
+         */
+        source.subscribe(getSecondObserver());
+
+        source.onNext(4);
+        source.onComplete();
+
+    }
+
 
     private Observer<Integer> getFirstObserver() {
         return new Observer<Integer>() {
